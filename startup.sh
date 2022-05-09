61bf7672ccb231d0fca5206d63afc21cec5744f7
kaylahoffman <64444491+kaylahoffman@users.noreply.github.com>
2022-05-08 20:38:31 -0400

    Delete startup.sh

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export mongodb_URI='mongodb+srv://kaylahoffman:kaylahoffmanCS103@cluster0.ofpxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-echo "connecting to $mongodb_URI"
-nodemon
